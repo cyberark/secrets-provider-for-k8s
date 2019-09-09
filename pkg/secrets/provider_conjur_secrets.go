45f3663a177df39971b0f91bb16de7c8dc9f72aa
orenbm <oren.benmeir@cyberark.com>
2019-09-09 16:49:58 +0300

    Refactored pushConjurSecrets to provideConjurSecrets & fixed description

--- pkg/secrets/provider_conjur_secrets.go
+++ pkg/secrets/provider_conjur_secrets.go
@@ -1,7 +1,7 @@
 package secrets
 
 /*
-	Struct implementing this interface provide Conjur secrets to a storage. For example, ProvideConjurSecretsToK8sSecrets
+	Structs implementing this interface provide Conjur secrets to a storage. For example, ProvideConjurSecretsToK8sSecrets
 	retrieves Conjur secrets that are required by the pod and pushes them into K8s secrets.
 */
 type ProvideConjurSecrets interface {
