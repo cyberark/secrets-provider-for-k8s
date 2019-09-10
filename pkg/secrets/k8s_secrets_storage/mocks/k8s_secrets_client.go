7226661cbf217f467d9d6f7757d5dde1e8c0ab95
orenbm <oren.benmeir@cyberark.com>
2019-09-10 12:37:56 +0300

    Remove MockK8sDB from MockK8sSecretsClient
    As we don't need to have a DB per Client we can have it as a global var

--- pkg/secrets/k8s_secrets_storage/mocks/k8s_secrets_client.go
+++ pkg/secrets/k8s_secrets_storage/mocks/k8s_secrets_client.go
@@ -3,16 +3,14 @@ package mocks
 import "github.com/cyberark/cyberark-secrets-provider-for-k8s/pkg/secrets/clients/k8s"
 
 // Mocks a K8s secrets client. Uses a mock K8s database to retrieve & patch secrets.
-type MockK8sSecretsClient struct {
-	MockK8sDB map[string]MockK8sSecret
-}
+type MockK8sSecretsClient struct{}
 
 func (k8sSecretsClient MockK8sSecretsClient) RetrieveK8sSecret(_ string, secretName string) (k8s.K8sSecretInterface, error) {
-	return k8sSecretsClient.MockK8sDB[secretName], nil
+	return MockK8sDB[secretName], nil
 }
 
 func (k8sSecretsClient *MockK8sSecretsClient) PatchK8sSecret(_ string, secretName string, stringDataEntriesMap map[string][]byte) error {
-	secretToPatch := k8sSecretsClient.MockK8sDB[secretName]
+	secretToPatch := MockK8sDB[secretName]
 	for key, value := range stringDataEntriesMap {
 		secretToPatch.Data[key] = value
 	}
