5c3df77be73a7dd108872d2577d6056a24c1d144
orenbm <oren.benmeir@cyberark.com>
2019-09-11 11:34:29 +0300

    Refactored ProvideConjurSecretsInterface to ProvideConjurSecrets so it won't have the redundant `Interface` suffix

--- pkg/secrets/provide_conjur_secrets.go
+++ pkg/secrets/provide_conjur_secrets.go
@@ -4,6 +4,6 @@ package secrets
 	Structs implementing this interface provide Conjur secrets to a storage. For example, ProvideConjurSecretsToK8sSecrets
 	retrieves Conjur secrets that are required by the pod and pushes them into K8s secrets.
 */
-type ProvideConjurSecretsInterface interface {
+type ProvideConjurSecrets interface {
 	Run() error
 }
