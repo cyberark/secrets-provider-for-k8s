90ec38ba07a8dfa882d814ba6e40c82a979b39b5
“Nessi <Nessi.Lahav@cyberark.com>
2019-09-16 17:26:36 +0300

    Add summon configuration to fetch OpenShift credentials from sas Conjur v4

---
+++ test/test_with_summon.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -xeuo pipefail
+
+source bootstrap.env
+
+./login_openshift.sh
+
+./0_deploy_conjur_on_kubernetes.sh
+
+./1_check_dependencies.sh
+
+./stop
+
+./2_create_test_app_namespace.sh
+
+if [[ "${DEPLOY_MASTER_CLUSTER}" = "true" ]]; then
+  ./3_load_conjur_policies.sh
+  ./4_init_conjur_cert_authority.sh
+fi
+
+./5_build_cyberark_secrets_provider_image.sh
+
+./6_deploy_test_env.sh
+
+exit_code=1
+for n in {1..5}; do
+  pod_name=$(oc get pods --namespace=$TEST_APP_NAMESPACE_NAME --selector app=test-env --no-headers | awk '{print $1}')
+  if [[ "$(oc logs $pod_name)" == "supersecret" ]]; then
+    exit_code=0
+    break
+  else
+    sleep 5
+  fi
+done
+
+./stop
+./kubernetes-conjur-deploy-"$UNIQUE_TEST_ID"/stop
+rm -rf "kubernetes-conjur-deploy-$UNIQUE_TEST_ID"
+
+exit $exit_code
