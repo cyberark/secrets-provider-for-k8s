c18a2dd0b611595a1dc070c4b8dd280f085f5bc0
orenbm <oren.benmeir@cyberark.com>
2019-09-22 14:30:05 +0300

    Build local image in script instead of in another file
    It is simpler to have this code in the main code with a description rather
    than having another file as we have very little logic in it.
    
    Removing this file also changed the name of the following file in the script
    order

--- test/5_build_cyberark_secrets_provider_image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ..
-./bin/build
-cd test
--- test/test_with_summon.sh
+++ test/test_with_summon.sh
@@ -14,9 +14,12 @@ if [[ "${DEPLOY_MASTER_CLUSTER}" = "true" ]]; then
   ./4_init_conjur_cert_authority.sh
 fi
 
-./5_build_cyberark_secrets_provider_image.sh
+  # build cyberark-secrets-provider image
+  pushd ..
+    ./bin/build
+  popd
 
-./6_deploy_test_env.sh
+./5_deploy_test_env.sh
 
 exit_code=1
 for n in {1..5}; do
