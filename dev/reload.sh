3bc1720c9394f0b94c06c3c4fbfe34514060b255
sigalsax <ssax18@gmail.com>
2020-07-06 16:14:24 +0300

    Fix reload script to easy rebuild and repush secrets provider image to remote environment
    - The reload script allows the developer to work locally and push their work to the remote clusters with a simple reload script.

--- dev/reload.sh
+++ dev/reload.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 set -xeuo pipefail
 
+. utils.sh
 # Script for making it easy to make a change locally and redeploy
-pushd ../bin
-  ./build
-popd
+cd ..
+./bin/build
+
+cd dev
+set_namespace $APP_NAMESPACE_NAME
 
 docker tag "secrets-provider-for-k8s:dev" \
          "${DOCKER_REGISTRY_PATH}/${APP_NAMESPACE_NAME}/secrets-provider"
 docker push "${DOCKER_REGISTRY_PATH}/${APP_NAMESPACE_NAME}/secrets-provider"
 
-echo "Running Deployment Manifest"
-wait_for_it 600 "$ENV_DIR/app-env.sh.yml | $cli_without_timeout apply -f -"
+selector="role=follower"
+cert_location="/opt/conjur/etc/ssl/conjur.pem"
+if [ "$CONJUR_DEPLOYMENT" = "oss" ]; then
+    selector="app=conjur-cli"
+    cert_location="/root/conjur-${CONJUR_ACCOUNT}.pem"
+fi
+conjur_pod_name=$($cli_with_timeout get pods --selector=$selector --namespace $CONJUR_NAMESPACE_NAME --no-headers | awk '{ print $1 }' | head -1)
+ssl_cert=$($cli_with_timeout "exec ${conjur_pod_name} --namespace $CONJUR_NAMESPACE_NAME cat $cert_location")
 
-$cli_with_timeout "get pods --namespace=$APP_NAMESPACE_NAME --selector app=test-app --no-headers | wc -l"
+export CONJUR_SSL_CERTIFICATE=$ssl_cert
+
+export ENV_DIR="$PWD/config/k8s"
+if [[ "$PLATFORM" = "openshift" ]]; then
+    export ENV_DIR="$PWD/config/openshift"
+fi
+
+deploy_env
