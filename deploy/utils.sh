a971a60914f9dca9b4fb1a738cbc4e4854ac95b3
sigalsax <ssax18@gmail.com>
2020-08-18 17:20:48 +0300

    Add Helm prop for test setup
    - Adds props for Helm tests including create Conjur and K8s/Openshift resources for test use
    - Adds ci folder for overriding Helm chart defaults dynamically

--- deploy/utils.sh
+++ deploy/utils.sh
@@ -116,6 +116,7 @@ runDockerCommand() {
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ~/.config:/root/.config \
     -v ~/.docker:/root/.docker \
+    -v "$PWD/../helm":/helm \
     -v "$PWD":/src \
     -w /src \
     $TEST_RUNNER_IMAGE:$CONJUR_NAMESPACE_NAME \
@@ -154,6 +155,147 @@ configure_conjur_url() {
 
   export CONJUR_APPLIANCE_URL=$conjur_appliance_url
   export CONJUR_AUTHN_URL=$conjur_authenticator_url
+}
+
+fetch_ssl_from_conjur() {
+  selector="role=follower"
+  cert_location="/opt/conjur/etc/ssl/conjur.pem"
+  if [ "$CONJUR_DEPLOYMENT" = "oss" ]; then
+    selector="app=conjur-cli"
+    export cert_location="/root/conjur-${CONJUR_ACCOUNT}.pem"
+  fi
+
+  export conjur_pod_name=$($cli_with_timeout get pods --selector=$selector --namespace $CONJUR_NAMESPACE_NAME --no-headers | awk '{ print $1 }' | head -1)
+}
+
+setup_helm_environment() {
+  set_namespace $CONJUR_NAMESPACE_NAME
+
+  configure_conjur_url
+
+  fetch_ssl_from_conjur
+
+  ssl_location="conjur.pem"
+  if [ "${DEV}" = "true" ]; then
+    ssl_location="../conjur.pem"
+  fi
+  # Save cert for later setting in Helm
+  $cli_with_timeout "exec ${conjur_pod_name} --namespace $CONJUR_NAMESPACE_NAME cat $cert_location" > "$ssl_location"
+
+  set_namespace $APP_NAMESPACE_NAME
+}
+
+set_image_path() {
+  image_path="${APP_NAMESPACE_NAME}"
+  if [[ "${PLATFORM}" = "openshift" && "${DEV}" = "false" ]]; then
+    # Image path needs to point to internal registry path to access image
+    image_path="docker-registry.default.svc:5000/${APP_NAMESPACE_NAME}"
+  elif [[ "${PLATFORM}" = "kubernetes" && "${DEV}" = "false" ]]; then
+    image_path="${DOCKER_REGISTRY_PATH}/${APP_NAMESPACE_NAME}"
+  fi
+  export image_path
+}
+
+fill_helm_chart() {
+  helm_path="."
+  if [ "${DEV}" = "false" ]; then
+    helm_path=".."
+  fi
+
+  set_image_path
+
+  id=${1:-""}
+  sed -e "s#{{ SECRETS_PROVIDER_ROLE }}#${SECRETS_PROVIDER_ROLE:-"secrets-provider-role"}#g" \
+    -e "s#{{ SECRETS_PROVIDER_ROLE_BINDING }}#${SECRETS_PROVIDER_ROLE_BINDING:-"secrets-provider-role-binding"}#g" \
+    -e "s#{{ CREATE_SERVICE_ACCOUNT }}#${CREATE_SERVICE_ACCOUNT:-"true"}#g" \
+    -e "s#{{ SERVICE_ACCOUNT }}#${SERVICE_ACCOUNT:-"secrets-provider-service-account"}#g" \
+    -e "s#{{ K8S_SECRETS }}#${K8S_SECRETS:-"test-k8s-secret"}#g" \
+    -e "s#{{ CONJUR_ACCOUNT }}#${CONJUR_ACCOUNT:-"cucumber"}#g" \
+    -e "s#{{ CONJUR_APPLIANCE_URL }}#${CONJUR_APPLIANCE_URL:-"https://conjur-follower.${CONJUR_NAMESPACE_NAME}.svc.cluster.local/api"}#g" \
+    -e "s#{{ CONJUR_AUTHN_URL }}#${CONJUR_AUTHN_URL:-"https://conjur-follower.${CONJUR_NAMESPACE_NAME}.svc.cluster.local/api/authn-k8s/${AUTHENTICATOR_ID}"}#g" \
+    -e "s#{{ CONJUR_AUTHN_LOGIN }}# ${CONJUR_AUTHN_LOGIN:-"host/conjur/authn-k8s/${AUTHENTICATOR_ID}/apps/${APP_NAMESPACE_NAME}/*/*"}#g"  \
+    -e "s#{{ SECRETS_PROVIDER_SSL_CONFIG_MAP }}# ${SECRETS_PROVIDER_SSL_CONFIG_MAP:-"secrets-provider-ssl-config-map"}#g" \
+    -e "s#{{ IMAGE_PULL_POLICY }}# ${IMAGE_PULL_POLICY:-"IfNotPresent"}#g" \
+    -e "s#{{ IMAGE }}# ${IMAGE:-"$image_path/secrets-provider"}#g" \
+    -e "s#{{ TAG }}# ${TAG:-"latest"}#g" \
+    -e "s#{{ LABELS }}# ${LABELS:-"app: test-helm"}#g" \
+    -e "s#{{ DEBUG }}# ${DEBUG:-"false"}#g" \
+    -e "s#{{ RETRY_COUNT_LIMIT }}# ${RETRY_COUNT_LIMIT:-"3"}#g" \
+    -e "s#{{ RETRY_INTERVAL_SEC }}# ${RETRY_INTERVAL_SEC:-"30"}#g" \
+    "$helm_path/helm/secrets-provider/ci/test-values-template.yaml" > "$helm_path/helm/secrets-provider/ci/${id}test-values.yaml"
+}
+
+fill_helm_chart_no_override_defaults() {
+  sed -e "s#{{ K8S_SECRETS }}#${K8S_SECRETS}#g" \
+    -e "s#{{ CONJUR_ACCOUNT }}#${CONJUR_ACCOUNT}#g" \
+    -e "s#{{ LABELS }}# ${LABELS}#g" \
+    -e "s#{{ CONJUR_APPLIANCE_URL }}#${CONJUR_APPLIANCE_URL}#g" \
+    -e "s#{{ CONJUR_AUTHN_URL }}#${CONJUR_AUTHN_URL}#g" \
+    -e "s#{{ CONJUR_AUTHN_LOGIN }}# ${CONJUR_AUTHN_LOGIN}#g"  \
+    "../helm/secrets-provider/ci/take-default-test-values-template.yaml" > "../helm/secrets-provider/ci/take-default-test-values.yaml"
+}
+
+fill_helm_chart_test_image() {
+  sed -e "s#{{ IMAGE }}#${IMAGE}#g" \
+    -e "s#{{ TAG }}#${TAG}#g" \
+    -e "s#{{ IMAGE_PULL_POLICY }}#${IMAGE_PULL_POLICY}#g" \
+    "../helm/secrets-provider/ci/take-image-values-template.yaml" > "../helm/secrets-provider/ci/take-image-values.yaml"
+}
+
+deploy_chart() {
+  pushd ../
+    fill_helm_chart
+    helm install -f "helm/secrets-provider/ci/test-values.yaml" \
+      secrets-provider ./helm/secrets-provider \
+      --set-file environment.conjur.sslCertificate.value="conjur.pem"
+  popd
+}
+
+set_config_directory_path() {
+  export CONFIG_DIR="config/k8s"
+  if [[ "$PLATFORM" = "openshift" ]]; then
+    export CONFIG_DIR="config/openshift"
+  fi
+}
+
+deploy_helm_app() {
+  set_config_directory_path
+
+  helm_app_path="../$CONFIG_DIR/helm-app.yaml"
+  if [ "${DEV}" = "true" ]; then
+      helm_app_path="test/$CONFIG_DIR/helm-app.yaml"
+  fi
+
+  id=${1:-""}
+  sed -e "s#{{ SERVICE_ACCOUNT }}#${SERVICE_ACCOUNT:-"secrets-provider-service-account"}#g" $helm_app_path |
+  sed -e "s#{{ K8S_SECRET }}#${K8S_SECRET:-"test-k8s-secret"}#g" |
+  sed -e "s#{{ ID }}#${id}#g" |
+  $cli_with_timeout create -f -
+}
+
+create_k8s_role() {
+  CONFIG_DIR="config/k8s"
+  if [[ "$PLATFORM" = "openshift" ]]; then
+    CONFIG_DIR="config/openshift"
+  fi
+
+  id=${1:-""}
+  sed -e "s#{{ ID }}#${id}#g" "../$CONFIG_DIR/secrets-access-role.yaml" |
+  sed -e "s#{{ APP_NAMESPACE_NAME }}#${APP_NAMESPACE_NAME}#g" |
+  $cli_with_timeout create -f -
+}
+
+create_k8s_secret_for_helm_deployment() {
+  helm_app_path="../config/k8s_secret.yml"
+  if [ "${DEV}" = "true" ]; then
+    helm_app_path="test/config/k8s_secret.yml"
+  fi
+
+  $cli_with_timeout create -f $helm_app_path
+}
+
+deploy_init_env() {
+  configure_conjur_url
 
   echo "Running Deployment Manifest"
 
@@ -232,7 +374,7 @@ test_secret_is_provided() {
   $cli_with_timeout "exec $conjur_cli_pod -- conjur variable values add \"$variable_name\" $secret_value"
 
   set_namespace "$APP_NAMESPACE_NAME"
-  deploy_env
+  deploy_init_env
 
   echo "Verifying pod test_env has environment variable '$environment_variable_name' with value '$secret_value'"
   pod_name=$(cli_get_pods_test_env | awk '{print $1}')
