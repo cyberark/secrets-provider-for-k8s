37bb1b684450e7d2a4e99108c093e03ea8646671
John ODonnell <john.odonnell@cyberark.com>
2021-09-23 13:10:31 -0400

    Update integration test cases

--- pkg/log/messages/error_messages.go
+++ pkg/log/messages/error_messages.go
@@ -65,7 +65,7 @@ const CSPFK043E string = "CSPFK043E Annotation '%s' does not accept value '%s': 
 const CSPFK044E string = "CSPFK044E Annotation '%s' must be provided"
 const CSPFK045E string = "CSPFK045E Annotation file line %d is malformed: expecting format \"<key>=<quoted value>\""
 
-const CSPFK046E string = "CSPFK046E Secret Store Type needs to be configured, either with SECRETS_DESTINATION environment variable or 'conjur.org/secrets-destination' Pod annotation"
+const CSPFK046E string = "CSPFK046E Secret Store Type needs to be configured, either with 'SECRETS_DESTINATION' environment variable or 'conjur.org/secrets-destination' Pod annotation"
 const CSPFK047E string = "CSPFK047E Secrets Provider in Push-to-File mode can only be configured with Pod annotations"
 const CSPFK048E string = "CSPFK048E Secrets Provider in K8s Secrets mode requires either the 'K8S_SECRETS' environment variable or 'conjur.org/k8s-secrets' Pod annotation"
 const CSPFK049E string = "CSPFK049E Failed to validate Pod annotations"
