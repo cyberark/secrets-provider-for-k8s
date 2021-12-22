9b361299a30b3060af46fbc34cb157dacc484c4c
Rob Pothier <Robert.Pothier@cyberark.com>
2021-12-22 14:03:19 -0500

    Revert to use the main branch of kubernetes-conjur-deploy
    Now that Conjur has been released with the we can backout
    the temporary changes.

--- deploy/test/test_in_docker.sh
+++ deploy/test/test_in_docker.sh
@@ -37,12 +37,7 @@ deployConjur() {
   # from inside the container
   docker pull $CONJUR_APPLIANCE_IMAGE
 
-  # Temporarily pull a branch that has Conjur edge
-  # due to a bug that is fixed but not yet released.
-  # This will be removed once Conjur is released with the fix.
-  # git clone git@github.com:cyberark/kubernetes-conjur-deploy \
-  #    kubernetes-conjur-deploy-$UNIQUE_TEST_ID
-  git clone --single-branch --branch downgrade-conjur-version git@github.com:cyberark/kubernetes-conjur-deploy \
+  git clone git@github.com:cyberark/kubernetes-conjur-deploy \
       kubernetes-conjur-deploy-$UNIQUE_TEST_ID
 
   cmd="./start"
