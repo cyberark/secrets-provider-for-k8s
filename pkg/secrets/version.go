eada629c5c2e6222f52f3c97467136ba008aba57
Samir Shetty <samir.bhatt@cyberark.com>
2024-07-24 09:06:51 -0700

    Bump version to 1.6.5

--- pkg/secrets/version.go
+++ pkg/secrets/version.go
@@ -3,7 +3,7 @@ package secrets
 import "fmt"
 
 // Version field is a SemVer that should indicate the baked-in version
-var Version = "1.6.4"
+var Version = "1.6.5"
 
 // Tag field denotes the specific build type for the broker. It may
 // be replaced by compile-time variables if needed to provide the git
